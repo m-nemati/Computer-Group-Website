ca07405ceab5cc12ad99c95e2351a2a34cf55e4c
m.nemati <m.nemati2002@gmail.com>
2018-11-21 14:52:24 +0330

    add sticky to nav bar

---
+++ js/myjscode.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        var scroll_Top=$(this).scrollTop();
+    
+        if(scroll_Top>200){
+            $("nav").addClass("fixNav");
+        }
+        else{
+            $("nav").removeClass("fixNav");
+        }
+    })
+})
