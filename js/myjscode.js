fd9e93e51587e421b47c27c4a1af4561402fc022
Mohsen Kordzangeneh <44491807+Union123123@users.noreply.github.com>
2018-11-30 12:59:04 -0800

    Update myjscode.js
    Edited by mohsen kordzangeneh : 
    خط های 9 و 13 و 22 تا 28 👍

--- js/myjscode.js
+++ js/myjscode.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
         var scroll_Top=$(this).scrollTop();
     
         if(scroll_Top>200){
+            $('#goTopButton').css('display','inline');
             $("nav").addClass("fixNav");
         }
         else{
+            $('#goTopButton').css('display','none');
             $("nav").removeClass("fixNav");
         }
     })
@@ -16,6 +18,13 @@ $(document).ready(function(){
 
     $(".dropbtn").click(function(){
         $(".dropdownContent").toggle();
+    });
+    $(document).ready(function(){
+
+        //Code For When Go To Top Button Click
+        $('#goTopButton').click(function(){
+            $('html').animate({scrollTop:0},1000);
+        });
     });
 
 })
